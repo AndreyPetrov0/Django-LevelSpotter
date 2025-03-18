b40d7fd5fc07b226271f2ca86f1e448218f5f671
Lenfer <andreypetrov1393@gmail.com>
2025-03-18 18:01:55 +0200

    Add use project (Level Spotter)

--- spotter/forms.py
+++ spotter/forms.py
@@ -1,4 +1,10 @@
 from django import forms
 
-class CreateNewList(forms.Form):
-    nameSSSS = forms.CharField(label="Name", max_length=200)
+class Correct_value(forms.Form):
+    correct_value = forms.CharField(label="Введіть корегувальне значення в мм.", max_length=200)
+
+
+class Number_value(forms.Form):
+    number_value = forms.CharField(label="Введіть значення на дісплею датчика", max_length=200)
+
+
--- spotter/views.py
+++ spotter/views.py
@@ -1,36 +1,46 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import CreateNewList
 
-lst_res = []
+lst_res = {'csale': 0, 'correct_value': 0, 'number_value': 0}
 
 def index(request):
+    if lst_res['csale'] != 0:
+        lst_res['csale'] = 0
+        lst_res['correct_value'] = 0
+        lst_res['number_value'] = 0
+        print(lst_res)
+
     print(request.GET, 'index')
     return render(request, 'spotter/index.html')
 
 def  index_1(request):
-    if request.method == "get":
-        print('pass')
+    if lst_res['csale'] == 0:
+        lst_res['csale'] = int(request.GET['csale'])
+        print(lst_res, 'index_1 IF')
     else:
-        form = CreateNewList()
-        lst_res.append(request.GET)
-        print(lst_res)
-        print('2')
+        lst_res['correct_value'] = 0
+        print(lst_res, 'index_1 ELSE')
 
     print(request.GET, 'index_1')
-    return render(request, 'spotter/index_1.html', {"form": form})
+    return render(request, 'spotter/index_1.html')
 
 def  index_2(request):
-    if request.method == "get":
-        print('pass')
+    if lst_res['correct_value'] == 0:
+        lst_res['correct_value'] = int(request.GET['correct_value'])
+        print(lst_res, 'index_2 IF')
     else:
-        form = CreateNewList()
-        lst_res.append(request.GET)
-        print(lst_res)
-        print('2')
+        lst_res['number_value'] = 0
+        print(lst_res, 'index_2 ELSE')
 
     print(request.GET, 'index_2')
     return render(request, 'spotter/index_2.html')
 
 def  index_3(request):
+    if lst_res['number_value'] == 0:
+        lst_res['number_value'] = int(request.GET['number_value'])
+        print(lst_res, 'index_3 IF')
+    else:
+        pass
+
+    print(request.GET, 'index_3')
     return render(request, 'spotter/index_3.html')
