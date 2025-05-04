b36a9e267336770aae495273d87157522abdd000
Lenfer <113467858+AndreyPetrov0@users.noreply.github.com>
2025-05-04 16:11:48 +0300

    Update settings.py

--- levelspotter/settings.py
+++ levelspotter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--0kr#yj8lfi=a%w)vc^kvzhiu))vllif4np-f(iv7f_kq0b&4s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
