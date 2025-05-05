c6f64a1341a39509ef69c1921202fafd18065151
Lenfer <113467858+AndreyPetrov0@users.noreply.github.com>
2025-05-05 17:56:59 +0300

    Update settings.py

--- levelspotter/settings.py
+++ levelspotter/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'spotter.apps.SpotterConfig',
     'levelspotter',
 ]
