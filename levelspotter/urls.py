ef34a6d5950a7c24132811d86642465c8d95e5f5
Lenfer <113467858+AndreyPetrov0@users.noreply.github.com>
2025-05-07 12:17:52 +0300

    Update urls.py

--- levelspotter/urls.py
+++ levelspotter/urls.py
@@ -23,5 +23,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("spotter.urls")),
+    path('', include("spotter.urls"), name='spotter'),
+    path('/download', index, name='download'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
